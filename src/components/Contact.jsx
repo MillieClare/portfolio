427f0600bdb3fd0a99d82638b4c724ea92625ac2
Millie Clare <Millie_Clare@outlook.com>
2024-03-03 01:54:11 +0000

    added in moving stars, got email to work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import { emailJs } from "../emailjs/details";
 
 const Contact = () => {
   const formRef = useRef();
@@ -16,8 +17,47 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        emailJs.service,
+        emailJs.template,
+        {
+          from_name: form.name,
+          to_name: "Millie",
+          from_email: form.email,
+          to_email: "millie_clare@outlook.com",
+          message: form.message,
+        },
+        emailJs.user
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible!");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+
+          console.log(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
